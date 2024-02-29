a9c078fa29764cb59697b282af300f36883ec91b
Jung SeokYoung <thrdud0423@gmail.com>
2024-02-29 19:24:47 +0900

    [INFRA]: init

---
+++ CoreAnimationEx/CoreAnimationEx/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  CoreAnimationEx
+//
+//  Created by jung on 2/29/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
+
